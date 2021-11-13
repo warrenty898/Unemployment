09c0873120e9ceee0a8b2cb84a3b5dd5aab0d6a9
warrenty898 <wrrntfft@gmail.com>
2021-11-13 18:07:54 -0500

    Got homepage working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/balls", to: "articles#index"
+  root "home_page#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
